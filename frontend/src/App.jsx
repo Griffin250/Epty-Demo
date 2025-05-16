4cd84c3f1fad34346c59c015cfe383e269f665c6
Griffin250 <griffinugofficial@gmail.com>
2025-05-16 14:23:15 +0200

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './components/Home'
+
+const App = () => {
+  return (
+    <div className="bg-gray-200 h-64">
+      <Home />
+    </div>
+  )
+}
+
+export default App
